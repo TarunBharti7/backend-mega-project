12f1d922de600eb705f38ec0872802d6be7aac64
Tarun Bharti <tarunbharti970@gmail.com>
2023-12-28 22:06:32 +0530

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("server test");
+// this method is used with common js
+// require('dotenv').config({path: './env'});
+
+// this method is used with module js
+import dotenv from "dotenv";
+dotenv.config({
+    path: './env'
+})
+import connectDB from "./db/index.js";
+
+
+
+
+connectDB();
+
