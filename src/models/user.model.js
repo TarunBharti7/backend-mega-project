44091905b31ff56a2ce2bcce3dc4469e46189f50
Tarun Bharti <tarunbharti970@gmail.com>
2023-12-30 21:26:40 +0530

    adding  bcryptjs encryption

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -48,5 +50,19 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+
+// encrypt the password using bcrypt js
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+// check the user password
+userSchema.methods.isPasswordCorrect  = async function (password){
+    return await bcrypt.compare(password , this.password)
+}
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
@@ -39,5 +40,7 @@ const videoSchema = new Schema(
         timestamps: true
     }
 ) 
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
